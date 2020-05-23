2dee105fcdf2315f76485eabce139bf77b7577b4
Liza Star <lizadstar18@gmail.com>
2020-05-23 10:17:08 -0500

    player deletes

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -9,24 +9,34 @@ class Team extends React.Component {
     team: [],
   }
 
-  componentDidMount() {
-    teamData.getPlayersbyUid(authData.getUid())
-      .then((team) => this.setState({ team }))
-      .catch((err) => console.error('unable to get all players', err));
-  }
+getInfo = () => {
+  teamData.getPlayersbyUid(authData.getUid())
+    .then((team) => this.setState({ team }))
+    .catch((err) => console.error('unable to get all players', err));
+}
 
-  render() {
-    const { team } = this.state;
-    const makeTeam = team.map((player) => <Player key={player.id} player={player} />);
-    return (
+componentDidMount() {
+  this.getInfo();
+}
+
+removePlayer = (playerId) => {
+  teamData.deletePlayer(playerId)
+    .then(() => this.getInfo())
+    .catch((err) => console.error('could not delete player', err));
+};
+
+render() {
+  const { team } = this.state;
+  const makeTeam = team.map((player) => <Player key={player.id} player={player} removePlayer={this.removePlayer} />);
+  return (
       <div className="Team">
         <h1> The GOAT Team </h1>
         <div className="d-flex flex-wrap">
           { makeTeam }
         </div>
       </div>
-    );
-  }
+  );
+}
 }
 
 export default Team;
